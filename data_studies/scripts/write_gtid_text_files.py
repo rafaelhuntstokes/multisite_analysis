490dd1ead82c4bd1f75fa558be8cb0cded2e299c
Rafael Hunt-Stokes <hunt-stokes@pplxint10.physics.ox.ac.uk>
2024-04-04 02:01:30 +0100

    update for gtid extraction of data events inside ROI.

--- data_studies/scripts/write_gtid_text_files.py
+++ data_studies/scripts/write_gtid_text_files.py
@@ -29,4 +29,34 @@ def get_bi214_gtids():
         np.savetxt(f"../extracted_data/bi214/gtid_lists/{irun}.txt", BI_IDS, fmt = "%i")
         print('Completed ', irun)
 
-get_bi214_gtids()
+def get_dataset_gtids():
+    """
+    Get all the GTIDS for the full dataset within the ROI for extraction from the full RATDS files.
+    """
+
+    run_list = np.loadtxt("../runlists/directionality_list.txt", dtype = int)
+    missing  = []
+    for irun in run_list:
+        gtids = []
+
+        # open the ntuple files for the dataset
+        try:
+            file = ROOT.TFile.Open(f"/data/snoplus3/hunt-stokes/clean_multisite/data_spectrum_runs_general_coincidence/output_{irun}.root")
+            ntuple = file.Get("clean_6m") # get the events inside the ROI for all FVs (incase I want to change this later on) with coincidence cuts ON
+        except:
+            missing.append(irun)
+            continue
+
+        for ientry in ntuple:
+            energy = ientry.energy
+            gtid   = ientry.gtid
+            if energy >= 2.5 and energy <= 5.0:
+                gtids.append(gtid)
+        
+        # now have all the gtids in 6 m not removed by coincidence tags and falling inside ROI --> write them to a file
+        np.savetxt(f"../extracted_data/full_analysis/gtids/{irun}.txt", gtids, fmt = "%i")
+
+    print(f"Missing {len(missing)} runs: \n")
+    print(missing)
+get_dataset_gtids()
+# get_bi214_gtids()
