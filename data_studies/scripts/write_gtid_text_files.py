241d6ecd3e89a24c26758e772be9ea61280df867
rafaelhuntstokes <rafaelhuntstokes@gmail.com>
2024-06-09 15:26:20 +0100

    Extract gtids of tagged bi214 events.

--- data_studies/scripts/write_gtid_text_files.py
+++ data_studies/scripts/write_gtid_text_files.py
@@ -6,35 +6,40 @@ Script opens up the run-by-run BiPo2124 tagged NTUPLEs and creates a .txt file o
 """
 
 def get_bi214_gtids():
-    run_list = np.loadtxt("../runlists/contains_solar_candidates.txt", dtype = int)
+    run_list = np.loadtxt("../runlists/quiet_period.txt", dtype = int)
+    runs_present = []
+    num_events   = 0
     for irun in run_list:
         
         BI_IDS = []        
         # open ntuple file
         try:
-            TFILE = ROOT.TFile.Open(f"/data/snoplus3/hunt-stokes/clean_bipo/tagged_ntuples/run_by_run214/output_{irun}.root")
+            TFILE = ROOT.TFile.Open(f"/data/snoplus3/hunt-stokes/multisite_clean/data_studies/extracted_data/better_tagging/raw_tagging_output/{irun}.root")
         except:
             print(f"Missing run {irun}")
             continue
 
         # get the ntuple inside the file
-        ntuple = TFILE.Get("ntuple214")
+        ntuple = TFILE.Get("tag_info")
 
         for ientry in ntuple:
             # for every tagged BiPo event, extract the GTID of the Bi214 and Po214 separately
-            bi_id = ientry.gtid2
+            bi_id = ientry.gtid_bi
             BI_IDS.append(bi_id)
-
+        num_events += len(BI_IDS)
+        runs_present.append(irun)
         # save these gtid list for the run
-        np.savetxt(f"../extracted_data/bi214/gtid_lists/{irun}.txt", BI_IDS, fmt = "%i")
-        print('Completed ', irun)
+        np.savetxt(f"../extracted_data/better_tagging/bi214_gtids/{irun}.txt", BI_IDS, fmt = "%i")
 
+    np.savetxt("../runlists/better_tagged_bi214.txt", runs_present, fmt  = "%i")
+
+    print(f"Found {len(runs_present)} runs totalling {num_events} events.")
 def get_dataset_gtids():
     """
     Get all the GTIDS for the full dataset within the ROI for extraction from the full RATDS files.
     """
 
-    run_list = np.loadtxt("../runlists/directionality_list.txt", dtype = int)
+    run_list = np.loadtxt("../runlists/batch4_1_missing.txt", dtype = int)
     missing  = []
     for irun in run_list:
         gtids = []
@@ -52,11 +57,11 @@ def get_dataset_gtids():
             gtid   = ientry.gtid
             if energy >= 2.5 and energy <= 5.0:
                 gtids.append(gtid)
-        
+        print(f"Found {len(gtids)} for run {irun}.")
         # now have all the gtids in 6 m not removed by coincidence tags and falling inside ROI --> write them to a file
         np.savetxt(f"../extracted_data/full_analysis/gtids/{irun}.txt", gtids, fmt = "%i")
 
     print(f"Missing {len(missing)} runs: \n")
     print(missing)
-get_dataset_gtids()
-# get_bi214_gtids()
+# get_dataset_gtids()
+get_bi214_gtids()
