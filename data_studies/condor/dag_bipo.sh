d8d73b2bff8f24a6b96b14c47bd012544a9579d3
rafaelhuntstokes <rafaelhuntstokes@gmail.com>
2024-12-24 12:23:49 +0000

    Analysis and BiPo tagging dag files and scripts.

---
+++ data_studies/condor/dag_bipo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/hunt-stokes/rattus_novus.sh
+cd /data/snoplus3/hunt-stokes/multisite_clean/data_studies/scripts
+
+./bipo_tagger_systematic $1 $2
--- data_studies/scripts/dag_submission.py
+++ data_studies/scripts/dag_submission.py
@@ -184,5 +184,69 @@ def submit_reproc(jobs_per_layer):
             
             if ilayer != 0:
                 dagfile.write(f"PARENT BATCH_{ilayer - 1} CHILD BATCH_{ilayer}\n")
+
+def submit_bipo_tagging(jobs_per_layer, out_dir):
+    """
+    Creates a DAG to submit BiPo tagging over entire directionality list (or whatever
+    list). Created for the Tl208 constraint systematic study.
+    """
+
+    # load the run list - 1 job / node per run
+    runlist = np.loadtxt("../runlists/quiet_period.txt", dtype = int)
+
+    # define the workin directory for log files and suchlike
+    working_dir = "/data/snoplus3/hunt-stokes/multisite_clean/data_studies"
+
+    # work out the number of layers
+    num_layers = int(len(runlist) / jobs_per_layer)
+
+    # add final layer containing the remainder
+    if len(runlist) % jobs_per_layer != 0:
+        num_layers += 1
+
+    print(f"Creating DAG with {num_layers} layers of {jobs_per_layer} jobs. Final layer has {len(runlist) % jobs_per_layer} jobs.")
+
+    # create a .txt file containing input arguments for each job in each layer
+    for ilayer in range(num_layers):
+        
+        with open(f"../condor/dag_arguments_bipo/layer_{ilayer}_args.txt", "w") as argsfile:
+            
+            # each line is the arguments for a given job
+            if ilayer != num_layers - 1:
+                for ijob in range(jobs_per_layer):
+                    run_idx = ilayer * jobs_per_layer + ijob
+
+                    argsfile.write(f"{runlist[run_idx]} {out_dir}\n")
+            else:
+                for ijob in range(len(runlist) % jobs_per_layer):
+                    run_idx = ilayer * jobs_per_layer + ijob
+
+                    argsfile.write(f"{runlist[run_idx]} {out_dir}\n")
+
+    
+    # create a .submit file for each job in each layer
+    for ilayer in range(num_layers):
+
+        with open(f"../condor/dag_submit_bipo/layer_{ilayer}.sub", "w") as layerfile:
+            layerfile.write(f"executable = {working_dir}/condor/dag_bipo.sh\n")
+            layerfile.write(f"output     = {working_dir}/condor/dag_output_bipo/layer_{ilayer}_$(Process).out\n")
+            layerfile.write(f"error      = {working_dir}/condor/dag_error_bipo/layer_{ilayer}_$(Process).err\n")
+            layerfile.write(f"log        = {working_dir}/condor/dag_log_bipo/layer_{ilayer}_$(Process).log\n")
+            layerfile.write("max_retries = 3\n")
+            layerfile.write(f"queue arguments from {working_dir}/condor/dag_arguments_bipo/layer_{ilayer}_args.txt\n")
+
+    
+    # create the .dag file and specify the dependencies between layers
+    with open("../condor/bipo_dag.dag", "w") as dagfile:
+
+        for ilayer in range(num_layers):
+
+
+            dagfile.write(f"JOB BATCH_{ilayer} {working_dir}/condor/dag_submit_bipo/layer_{ilayer}.sub\n")
+            
+            if ilayer != 0:
+                dagfile.write(f"PARENT BATCH_{ilayer - 1} CHILD BATCH_{ilayer}\n")
+
+submit_bipo_tagging(50, "/data/snoplus3/hunt-stokes/multisite_clean/data_studies/extracted_data/full_analysis3/systematic_tagged212")
 # submit_reproc(50)
 # submit_analysis(50, 6000.0, -6000.0)
